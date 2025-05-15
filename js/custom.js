e6247d2b64eff68087e46906f29e80ce7bf7cad8
ryan-zzlove1 <96757109+ryan-zzlove1@users.noreply.github.com>
2025-05-15 23:54:27 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,10 @@
 
+$(document).ready(function() {
+    setTimeout(function() {
+        selectors.toggleSelector('keywords', 'classical-cnn');
+        selectors.toggleSelector('keywords', 'enhanced-cnn');
+        selectors.toggleSelector('keywords', 'transformer-based');
+        
+        page.updateShowPart();
+    }, 500); 
+});
